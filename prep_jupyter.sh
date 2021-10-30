b28d9ab2fdc6480c60f4b95faaf4bfb04205a998
PHAB1 <pedroa_barros1@hotmail.com>
2021-10-30 15:11:41 -0300

    Update prep_jupyter.sh

--- prep_jupyter.sh
+++ prep_jupyter.sh
@@ -22,7 +22,6 @@ conda install pandas numpy -y
 conda install -c bioconda sra-tools -y
 conda install -c bioconda entrez-direct -y
 conda install -c bioconda fastqc -y
-conda install -c bioconda fastp -y
 conda install -c bioconda quast -y
 conda install -c bioconda htseq -y
 conda install -c bioconda seqtk -y
@@ -34,6 +33,6 @@ R -e "install.packages('BiocManager',repos='https://cran.ma.imperial.ac.uk/')"
 R -e "install.packages('IRkernel')"
 R -e "IRkernel::installspec()"
 
-R -e "BiocManager::install(c('limma','edgeR','Glimma','org.Mm.eg.db', 'statmod'))"
+R -e "BiocManager::install(c('limma','edgeR','Glimma','org.Hs.eg.db', 'statmod'))"
 
 pip3 install IPython
